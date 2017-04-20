d1d23a0965e1637ed3e892541cb301a83901e9b2
15017206 <15017206@myrp.edu.sg>
2017-04-21 01:21:15 +0800

    Update

--- app/src/main/java/com/example/a15017206/p01_dailygoals/P01_DailyGoals.java
+++ app/src/main/java/com/example/a15017206/p01_dailygoals/P01_DailyGoals.java
@@ -1,6 +1,7 @@
 package com.example.a15017206.p01_dailygoals;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,8 +13,76 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import static android.R.attr.x;
+//import static com.example.a15017206.p01_dailygoals.R.id.radioButton;
+import static com.example.a15017206.p01_dailygoals.R.id.radioButton1;
+import static com.example.a15017206.p01_dailygoals.R.id.radioButton2;
+import static com.example.a15017206.p01_dailygoals.R.id.radioButton3;
+import static com.example.a15017206.p01_dailygoals.R.id.radioButton4;
+import static com.example.a15017206.p01_dailygoals.R.id.radioButton5;
+import static com.example.a15017206.p01_dailygoals.R.id.radioButton6;
 
 public class P01_DailyGoals extends AppCompatActivity {
+
+    String a = "No";
+    String b = "No";
+    String c = "No";
+    String d;
+
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getBaseContext(), "onPaused", Toast.LENGTH_SHORT).show();
+
+        //Shared Preferences
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("key1", MODE_PRIVATE);
+        SharedPreferences.Editor e = sp.edit();
+        e.putString("key-rb1",a);
+        e.putString("key-rb2",b);
+        e.putString("key-rb3",c);
+        e.putString("key-et4",d);
+        e.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Shared Preferences
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("key1", MODE_PRIVATE);
+        SharedPreferences.Editor e = sp.edit();
+
+        EditText et1 = (EditText) findViewById(R.id.editText2);
+        et1.setText(sp.getString("key-et4",null));
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+        RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+        RadioGroup rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
+
+
+
+
+        if (sp.getString("key-rb1", null).equalsIgnoreCase("Yes")){
+            Toast.makeText(getBaseContext(), "Test", Toast.LENGTH_SHORT).show();
+            rg.check(radioButton1);
+        }else {
+            rg.check(radioButton2);
+        }
+
+        if (sp.getString("key-rb2", null).equalsIgnoreCase("Yes")){
+            rg2.check(radioButton3);
+        }else {
+            rg2.check(radioButton4);
+        }
+
+        if (sp.getString("key-rb3", null).equalsIgnoreCase("Yes")){
+            rg3.check(radioButton5);
+        }else {
+            rg3.check(radioButton6);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +109,7 @@ public class P01_DailyGoals extends AppCompatActivity {
                 RadioButton rb2 = (RadioButton) findViewById(selBtnId2);
                 RadioButton rb3 = (RadioButton) findViewById(selBtnId3);
 
-                Toast.makeText(getBaseContext(), rb1.getText(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getBaseContext(), rb1.getText(), Toast.LENGTH_SHORT).show();
 
                 //Put radio button options & EditText into an array
                 String[] radioGroupEditText = {rb1.getText().toString(), rb2.getText().toString(),rb3.getText().toString(), et1.getText().toString()};
@@ -50,8 +119,16 @@ public class P01_DailyGoals extends AppCompatActivity {
 
                 startActivity(i);
 
+                //Shared Preferences
+                SharedPreferences sp = getApplicationContext().getSharedPreferences("key1", MODE_PRIVATE);
+                SharedPreferences.Editor e = sp.edit();
 
+                a = rb1.getText().toString();
+                b = rb2.getText().toString();
+                c = rb3.getText().toString();
+                d = et1.getText().toString();
 
+//                et1.setText(sp.getString("key-et4",null));
             }
         });
     }
--- app/src/main/java/com/example/a15017206/p01_dailygoals/P01_DailyGoals_2.java
+++ app/src/main/java/com/example/a15017206/p01_dailygoals/P01_DailyGoals_2.java
@@ -3,6 +3,8 @@ package com.example.a15017206.p01_dailygoals;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class P01_DailyGoals_2 extends AppCompatActivity {
@@ -13,8 +15,9 @@ public class P01_DailyGoals_2 extends AppCompatActivity {
         setContentView(R.layout.activity_p01__daily_goals_2);
 
         TextView tv1 = (TextView) findViewById(R.id.textViewOutput);
+        Button btn2 = (Button) findViewById(R.id.button2);
 
-        Intent j = getIntent();
+         Intent j = getIntent();
         String[] receive2 = j.getStringArrayExtra("info");
 
         tv1.setText("Read up on materials before class: " + receive2[0] + "\n"
@@ -22,5 +25,12 @@ public class P01_DailyGoals_2 extends AppCompatActivity {
                 + "Attempt the problem myself: " + receive2[2] + "\n \n"
                 + "Reflection: " + receive2[3]
         );
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
